65d20c4e947bdac3d04f59562f2d3aa36f958a5c
DawisX13 <dawisx13@gmail.com>
2025-08-09 22:24:03 +0200

    Initial commit

---
+++ src/main/java/net/dawisx13/deathmod/DeathModClient.java
@@ -0,0 +1,10 @@
+package net.dawisx13.deathmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DeathModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/dawisx13/deathmod/DeathModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.dawisx13.deathmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DeathModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
